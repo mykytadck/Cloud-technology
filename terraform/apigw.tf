50d9c17453f52d37e8df96cbe5e2f9b9ed8cf1c0
20DCK22 <octavisiya666@gmail.com>
2022-06-10 05:49:57 +0300

    domen-2

---
+++ terraform/apigw.tf
@@ -0,0 +1,99 @@
+resource "aws_api_gateway_rest_api" "this" {
+  name                     = module.base_labels.id
+  description              = "API Gateway description"
+  binary_media_types       = ["UTF-8-encoded"]
+  minimum_compression_size = -1
+  api_key_source           = "HEADER"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+  policy = null
+}
+
+resource "aws_api_gateway_gateway_response" "this" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  status_code   = "404"
+  response_type = "UNAUTHORIZED"
+
+  response_templates = {
+    "application/json" = "{\"message\":$context.error.messageString}"
+  }
+  }
+resource "aws_api_gateway_deployment" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.this.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "dev" {
+  deployment_id = aws_api_gateway_deployment.this.id
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  stage_name    = "dev"
+}
+
+
+
+ resource "aws_api_gateway_resource" "authors" {
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+  path_part   = "authors"
+  rest_api_id = aws_api_gateway_rest_api.this.id
+}
+
+resource "aws_api_gateway_method" "get_authors" {
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.authors.id
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+}
+
+resource "aws_api_gateway_integration" "get_authors" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.authors.id
+  http_method             = aws_api_gateway_method.get_authors.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = module.lambda.get_all_authors_invoke_arn
+  request_parameters      = {"integration.request.header.X-Authorization" = "'static'"}
+  request_templates       = {
+    "application/xml" = <<EOF
+  {
+     "body" : $input.json('$')
+  }
+  EOF
+  }
+  content_handling = "CONVERT_TO_TEXT"
+}
+
+resource "aws_api_gateway_method_response" "get_authors" {
+  rest_api_id     = aws_api_gateway_rest_api.this.id
+  resource_id     = aws_api_gateway_resource.authors.id
+  http_method     = aws_api_gateway_method.get_authors.http_method
+  status_code     = "200"
+  response_models = { "application/json" = "Empty" }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = false
+  }
+}
+
+resource "aws_api_gateway_integration_response" "authors" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.authors.id
+  http_method = aws_api_gateway_method.get_authors.http_method
+  status_code = aws_api_gateway_method_response.get_authors.status_code
+
+  response_parameters ={
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+
